ac356a6aacf337063914f48608b184ccff98a9d3
Anton Serhiienko <anton.serhiienko@klarna.com>
2019-10-29 22:59:04 +0200

    added inserts

--- src/main/scala/fesl/skunk.scala
+++ src/main/scala/fesl/skunk.scala
@@ -5,32 +5,41 @@ import skunk._
 import skunk.implicits._
 import skunk.codec.all._
 import fs2.Stream.resource
-import skunk.data.Type
+import skunk.data.{Completion, Type}
+import cats.effect.implicits._
 
 import scala.util.Try
-class PgView[F[_], E] extends ViewTable[F, E] {
+class PgView[F[_], E](implicit s: Session[F], decoder: Decoder[E]) extends ViewTable[F, E] {
   override def insert(e: E): F[E]             = ???
   override def select(id: UUID): F[Option[E]] = ???
 }
 
-class PgLog[F[_], E](implicit s: Session[F], sqlDecoder: Decoder[E]) extends LogTable[F, E] {
-  val varchar: Codec[UUID] = Codec.simple(
-    _.toString,
-    x => Try(UUID.fromString(x)).toEither.left.map(_.getMessage),
-    Type.uuid)
+class PgLog[F[_], E](implicit s: Session[F], decoder: Decoder[E], encoder: Codec[E])
+    extends LogTable[F, E] {
+  val uuid: Codec[UUID] = Codec.simple(_.toString,
+                                       x => Try(UUID.fromString(x)).toEither.left.map(_.getMessage),
+                                       Type.uuid)
 
   val select: Query[UUID, E] =
     sql"""
-      select id, seq_nr, event, datetime
+      select id, seq_nr, event, hash, datetime
       from log
-      WHERE id =  $varchar
-    """.query(sqlDecoder)
+      WHERE id =  $uuid
+    """.query(decoder)
 
-  override def insert(e: E): F[E] = ???
+  val insertCity: Command[E] =
+    sql"""
+         INSERT INTO log
+         VALUES ($encoder)
+       """.command
+
+  override def insert(e: E): F[E] =
+    resource(s.prepare(insertCity)).map(_.execute(e)).compile.drain.as(e)
 
   override def select(id: UUID): fs2.Stream[F, E] =
     for {
       pq     <- resource(s.prepare(select))
       events <- pq.stream(id, 20)
     } yield events
+
 }
