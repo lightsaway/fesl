01c798a1b352f488b5d9ab3a3d5298951485e201
Anton Serhiienko <anton.serhiienko@klarna.com>
2019-10-30 13:50:36 +0200

    reshuffling sqls

--- src/main/scala/fesl/skunk.scala
+++ src/main/scala/fesl/skunk.scala
@@ -1,14 +1,14 @@
 package fesl
+import java.time.LocalDateTime
 import java.util.UUID
 
 import cats.effect.Bracket
-import skunk.{Codec, _}
+import skunk._
 import skunk.implicits._
 import skunk.codec.all._
 import fs2.Stream.resource
 import skunk.data.Type
 import cats.implicits._
-import cats.effect.implicits._
 import fesl.ExtractUUID.ExtractUUID
 import fesl.PgLog.LogEncoder
 import fesl.types.BracketThrow
@@ -19,65 +19,90 @@ object codecs {
   val uuid: Codec[UUID] = Codec.simple(_.toString,
                                        x => Try(UUID.fromString(x)).toEither.left.map(_.getMessage),
                                        Type.uuid)
+  val json: Codec[String] = ???
 }
 import codecs._
 
 object types {
   type BracketThrow[F[_]] = Bracket[F, Throwable]
 }
+object PgView {
+  def createTable[F[_]: BracketThrow](implicit s: Session[F]): F[Unit] = {
+    val cmd: Command[Void] = sql"""
+                                   CREATE TABLE IF NOT EXISTS aggregate_view (
+                                                      id UUID PRIMARY KEY,
+                                                      state TEXT,
+                                                      updated_on TIMESTAMP WITH TIME ZONE
+                                )
+                     """.command
+    s.execute(cmd).void
+  }
+}
 
 class PgView[F[_]: BracketThrow, E](implicit s: Session[F],
-                                    decoder: Decoder[E],
-                                    encoder: Codec[E],
+                                    codec: Codec[E],
                                     extractUUID: ExtractUUID[E])
     extends ViewTable[F, E] {
 
   val selectQ: Query[UUID, E] =
     sql"""
-      select id, seq_nr, event, hash, datetime
-      from log
+      select * from aggregate_view
       WHERE id =  $uuid
-    """.query(decoder)
+    """.query(codec)
 
-  val insertQ: Command[UUID ~ E] =
+  val insertQ: Command[E] =
     sql"""
-          INSERT INTO view
-          VALUES ($uuid, $encoder, now())
+          INSERT INTO aggregate_view
+          VALUES ($codec)
           ON CONFLICT (id)
           DO UPDATE SET state = Excluded.state, datetime = Excluded.state;
        """.command
 
   override def insert(e: E): F[E] =
-    s.prepare(insertQ).use(_.execute(extractUUID(e) ~ e)).as(e)
+    s.prepare(insertQ).use(_.execute(e)).as(e)
 
   override def select(id: UUID): F[Option[E]] =
     s.prepare(selectQ).use(_.option(id))
 }
 
 object PgLog {
-  type LogEncoder[E] = E => java.util.UUID ~ Long ~ String ~ String
+  type LogRow        = java.util.UUID ~ Long ~ String ~ String ~ LocalDateTime
+  type LogEncoder[E] = E => LogRow
+
+  def decoder[E](f: LogRow => E) = (uuid ~ int8 ~ text ~ text ~ timestamp).map(f)
+  def deriveCodec[E]             = (uuid ~ int8 ~ text ~ text ~ timestamp).map(???)
+
+  def createTable[F[_]: BracketThrow](implicit s: Session[F]): F[Unit] = {
+    val cmd: Command[Void] = sql"""
+                                   CREATE TABLE IF NOT EXISTS event_log (
+                                                      id UUID PRIMARY KEY,
+                                                      agg_id UUID,
+                                                      seq_nr INTEGER
+                                                      e_type VARCHAR(50),
+                                                      event TEXT,
+                                                      appeared_on TIMESTAMP WITH TIME ZONE
+                                )
+                     """.command
+    s.execute(cmd).void
+  }
 }
 
 class PgLog[F[_]: BracketThrow, E](implicit s: Session[F],
-                                   decoder: Decoder[E],
-                                   encoder: LogEncoder[E],
+                                   codec: Codec[E],
                                    extractUUID: ExtractUUID[E])
     extends LogTable[F, E] {
 
-  // table schema id, seq_nr, event, hash, datetime
-
   val select: Query[UUID, E] =
     sql"""
-      select id, seq_nr, event, hash, datetime
-      from log
-      WHERE id =  $uuid
-    """.query(decoder)
+      select * from event_log
+      WHERE agg_id =  $uuid
+    """.query(codec)
 
   val insert: Command[E] =
     sql"""
          INSERT INTO log
-         VALUES ($uuid, $int8, $text, $text, now())
-       """.command.contramap(encoder)
+         VALUES ($codec)
+       """.command
 
   override def insert(e: E): F[E] =
     s.prepare(insert).use(_.execute(e)).as(e)
